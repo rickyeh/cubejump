8850e048aad6aa3895d8996946e947d83ca2a2de
RickYeh <RickYeh@users.noreply.github.com>
2015-06-05 15:04:40 -0700

    Changed title screen around to center title

--- title.js
+++ title.js
@@ -7,17 +7,18 @@ Title.prototype = {
         this.game.stage.backgroundColor = '#87CEEB';
 
         // Add title image
-        this.title = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY - 200, 'title');
+        this.title = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'title');
         this.title.anchor.set(0.5);
 
         // Add animating cube
-        this.cube = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY + 50, 'ninja');
+        this.cube = this.game.add.sprite(this.game.world.centerX - 60, this.game.world.centerY - 55, 'ninja');
         this.cube.anchor.set(0.5);
+        console.log(this.game.world.centerY);
 
         // Add tween animation to the cube
         this.tween = this.game.add.tween(this.cube)
-        .to({ y: 275}, 250, Phaser.Easing.Exponential.Out, false, 3000)
-        .to({ y: 425}, 400, Phaser.Easing.Exponential.In, true).loop().start();
+        .to({ y: 180}, 250, Phaser.Easing.Exponential.Out, false, 3000)
+        .to({ y: 320}, 400, Phaser.Easing.Exponential.In, true).loop().start();
 
         // Add button to start game
         this.button = this.game.add.button(this.game.world.centerX, this.game.world.centerY + 200, 'startButton', this.startClick, this);
