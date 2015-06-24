f2b010bc7679bc2c33a64bb0e0410291ed443628
RickYeh <RickYeh@users.noreply.github.com>
2015-06-23 19:55:30 -0700

    Adjust game window to center and have dark background

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var game = null; // Make it global so that it can be accessed by other JS files.
 
 (function() {
     game = new Phaser.Game(1334, 750, Phaser.AUTO, "gameScreen", {});
+    
     game.state.add("Preload", Preload);
     game.state.add("Title", Title);
     game.state.add("Main", Main);
