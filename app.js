50770bb5e9c29b544f6281d77c55f0a1c170dca8
RickYeh <RickYeh@users.noreply.github.com>
2015-07-02 19:07:05 -0700

    Added global varaibles object

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var game = null; // Make it global so that it can be accessed by other JS files.
 
+var globals = {
+    stage: 1
+};
+
 (function() {
     game = new Phaser.Game(1334, 750, Phaser.AUTO, "gameScreen", {});
-    
+
     game.state.add("Preload", Preload);
     game.state.add("Title", Title);
     game.state.add("Main", Main);
