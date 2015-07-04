5229fd56e94db3a174345f46909a12a9e932330b
RickYeh <RickYeh@users.noreply.github.com>
2015-07-03 18:10:24 -0700

    Made adjustments to spike placement to account for removed physics calculations in normal mode

--- level1.js
+++ level1.js
@@ -3,31 +3,31 @@ var Level1 = {
     start: function () {
         // 0
         this.createFloor(0, 10);
-        this.createSpike(2, 600, 2);
-        this.createSpike(4, 600, 4);
-        this.createSpike(6, 600, 6);
-        this.createSpike(8.03, 600, 8);
+        this.createSpike(2, 606, 2);
+        this.createSpike(4, 606, 4);
+        this.createSpike(6, 606, 6);
+        this.createSpike(8.03, 606, 8);
         // 10
         //this.createFloor(10, 1);
         this.createFloor(11, 12);
         this.createBrick(12, 500, 4);
         this.createBrick(12.5, 500, 4);
         this.createBrick(15, 500, 8);
-        this.createSpike(15, 600, 30);
+        this.createSpike(15, 606, 30);
         this.createBrick(16, 500, 8);
-        this.createSpike(18.5, 600, 16);
+        this.createSpike(18.5, 606, 16);
         this.createBrick(18.75, 500, 3);
         this.createBrick(19.25, 500, 3);
         // 20
-        this.createSpike(20.5, 600, 1);
-        this.createSpike(21.1, 600, 2);
-        this.createSpike(21.75, 600, 3);
-        this.createSpike(22.5, 600, 4);
+        this.createSpike(20.5, 606, 1);
+        this.createSpike(21.1, 606, 2);
+        this.createSpike(21.75, 606, 3);
+        this.createSpike(22.5, 606, 4);
         this.createBrick(23.25, 500, 6);
         this.createBrick(24, 400, 6);
         this.createFloor(25, 3);
-        this.createSpike(26, 600, 4);
-        this.createSpike(27, 600, 4);
+        this.createSpike(26, 606, 4);
+        this.createSpike(27, 606, 4);
         this.createBrick(28.25, 500, 8);
         this.createBrick(29.25, 400, 6);
         this.createBrick(30, 300, 4);
--- level2.js
+++ level2.js
@@ -3,24 +3,24 @@ var Level2 = {
     start: function () {
 
         this.createFloor(0, 3);
-        this.createSpike(2, 600, 3);
+        this.createSpike(2, 606, 3);
         this.createFloor(4);
-        this.createSpike(4.4, 600, 3);
+        this.createSpike(4.4, 606, 3);
         this.createFloor(6);
         this.createBrick(7.5, 500, 4);
         this.createBrick(8.25,500, 4);
         this.createFloor(9, 6);
         this.createBrick(9.7, 375, 4, 4);
-        this.createSpike(9.75, 600, 2);
+        this.createSpike(9.75, 606, 2);
 
         this.createBrick(10.45, 375, 4, 4);
-        this.createSpike(10.5, 600, 2);
+        this.createSpike(10.5, 606, 2);
         this.createBrick(11.2, 375, 4, 4);
-        this.createSpike(11.25, 600, 2);
-        this.createSpike(12, 600, 2);
-        this.createSpike(12.55, 600, 5);
-        this.createSpike(13.35, 600, 2);
-        this.createSpike(13.9, 600, 5);
+        this.createSpike(11.25, 606, 2);
+        this.createSpike(12, 606, 2);
+        this.createSpike(12.55, 606, 5);
+        this.createSpike(13.35, 606, 2);
+        this.createSpike(13.9, 606, 5);
         this.createFloor(14.5);
         this.createBrick(16, 500, 4);
         this.createBrick(16.75, 400, 4);
@@ -32,13 +32,13 @@ var Level2 = {
         this.createBrick(21, 300, 4);
         this.createBrick(22, 400, 4);
         this.createBrick(23, 500, 10);
-        this.createSpike(23.25, 400, 2);
+        this.createSpike(23.25, 468, 2); 
         this.createFloor(24, 3);
-        this.createSpike(24.5, 600);
-        this.createSpike(25, 600);
-        this.createSpike(25.5, 600);
-        this.createSpike(26, 600);
-        this.createSpike(26.5, 600, 3);
+        this.createSpike(24.5, 606);
+        this.createSpike(25, 606);
+        this.createSpike(25.5, 606);
+        this.createSpike(26, 606);
+        this.createSpike(26.5, 606, 3);
         this.createBrick(27.5, 500, 4);
         this.createBrick(28.25, 400, 4);
         this.createBrick(29, 300, 4);
@@ -46,7 +46,7 @@ var Level2 = {
         this.createBrick(30, 400, 4);
         this.createBrick(31, 400, 4);
         this.createBrick(32, 400, 10);
-        this.createSpike(32.3, 300, 2);
+        this.createSpike(32.3, 368, 2); 
         this.createBrick(33.5, 500, 4);
         this.createBrick(34.25, 400, 3);
         this.createBrick(35, 350, 3);
